c96635da22617996a8027e290b4772b3857b45b7
QuirinEcker <quirin.ecker@bajupa.com>
2021-11-09 15:05:58 +0100

    added feature to keep state on instance change

--- app/src/main/java/com/example/cooldroid/MainActivity.kt
+++ app/src/main/java/com/example/cooldroid/MainActivity.kt
@@ -2,24 +2,48 @@ package com.example.cooldroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     var counter = 0
+    private val COUNTER = "counter"
+    private val LOG_TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt(COUNTER)
+            setText()
+        }
         imageView.setOnClickListener {
             counter++
-            Log.d(null, "onCreate(), counter: ${counter}")
-            cooldroidText.text =
-                when (counter) {
-                    1 -> getString(R.string.once)
-                    2 -> getString(R.string.twice)
-                    else -> getString(R.string.text_view_numbers, counter)
-                }
+            setText()
         }
+    }
+
+    private fun setText() {
+        Log.d(null, "onCreate(), counter: ${counter}")
+        cooldroidText.text = getText()
+
+    }
+
+    private fun getText(): String {
+        print(counter)
+        return when (counter) {
+            1 -> getString(R.string.once)
+            2 -> getString(R.string.twice)
+            else -> getString(R.string.text_view_numbers, counter)
+        }
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(COUNTER, counter)
+        Log.d(LOG_TAG, "onSaveInstance(), counter: ${counter}")
+        super.onSaveInstanceState(outState)
     }
 }
